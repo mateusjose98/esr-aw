a5ee8bd4a128996147f8163f75d5b6906cce3916
José Mateus <josemateus.ufma@gmail.com>
2021-05-08 11:37:46 -0300

    Listando cozinhas com EntityManager

---
+++ src/main/java/com/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,23 @@
+package com.algafood.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Component;
+
+import com.algafood.domain.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	public List<Cozinha> listar(){
+		TypedQuery<Cozinha> query =  manager.createQuery("from Cozinha", Cozinha.class);
+		return query.getResultList();
+	}
+}

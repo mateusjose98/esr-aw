2e60b7c04c24adc1965761bf451115d09a76e28e
mateus <josemateus.ufma@gmail.com>
2022-02-05 14:06:51 -0300

    Projeto retomado

--- src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
@@ -16,8 +16,6 @@ public class CadastroEstadoService {
 	private static final String MSG_ESTADO_EM_USO 
 		= "Estado de código %d não pode ser removido, pois está em uso";
 	
-
-	
 	@Autowired
 	private EstadoRepository estadoRepository;
 	
@@ -30,7 +28,7 @@ public class CadastroEstadoService {
 			estadoRepository.deleteById(estadoId);
 			
 		} catch (EmptyResultDataAccessException e) {
-			throw new EstadoNaoEncontradoException(( estadoId ));
+			throw new EstadoNaoEncontradoException(estadoId);
 		
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
